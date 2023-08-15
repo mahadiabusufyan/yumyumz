bd11f31d67f126297ed14f259bad6955844ffbac
mahadiabusufyan <maabsuf@gmail.com>
2023-08-15 01:04:51 +0000

    complete hero

--- app/(routes)/components/Hero.tsx
+++ app/(routes)/components/Hero.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import recipe from '../../../public/assets/recipe.json';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const Hero = () => {
   const { user } = useAuth();
@@ -13,40 +14,53 @@ const Hero = () => {
   const [animationHeight, setAnimationHeight] = useState(400);
 
   useEffect(() => {
-    const screenWidth = window.innerWidth;
-    if (screenWidth <= 768) {
-      setAnimationHeight(300);
-    } else {
-      setAnimationHeight(400);
-    }
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 768) {
+        setAnimationHeight(170); // Small screens
+      } else if (screenWidth <= 1024) {
+        setAnimationHeight(250); // Tablet screens
+      } else {
+        setAnimationHeight(400); // Larger screens
+      }
+    };
+
+    // Initial calculation on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
+
   return (
-    <div className="w-full h-[70vh] lg:h-[60vh] flex items-center justify-center px-3">
-      <div className="bg-[#f3def9] h-full container rounded-3xl  relative px-3 flex flex-col items-center justify-center lg:flex-row w-full">
-        <div className="lg:w-1/2 text-center w-full lg:text-left flex flex-col items-center justify-center lg:pl-10 lg:items-start gap-5 h-full">
-          <h3 className="text-5xl lg:text-6xl font-medium">
-            The Recipe <br></br>Community
+    <div className="w-full h-[60vh] md:h-[70vh] flex items-center justify-center px-3">
+      <div className="bg-[#f3def9] h-full container mx-auto rounded-3xl flex flex-col lg:flex-row items-center justify-center p-5">
+        <div className="lg:w-1/2 text-center lg:text-left flex flex-col items-center justify-center lg:items-start gap-3 lg:pl-16">
+          <h3 className="text-5xl lg:text-6xl font-semibold">
+            The Recipe Community
           </h3>
-          <p className="text-xl">
+          <p className="text-base lg:text-lg">
             Cooking with Love ❤️, Sharing with the World.
           </p>
-          {user ? (
-            <div></div>
-          ) : (
-            <div className="grid gap-3">
-              <Button
-                onClick={() => router.push('/auth/register')}
-                label="Get started - It's free"
-              />
-            </div>
-          )}
+          <div className="mt-3 w-full flex items-center justify-center lg:justify-start">
+            <Button
+              icon={user ? AiOutlinePlus : null} // Conditional icon rendering
+              onClick={() => router.push('/auth/register')}
+              label={user ? 'Add new recipe' : "Get started - It's free"}
+            />
+          </div>
         </div>
-        <div className="items-center justify-center flex">
-          <div className="mx-auto -mt-5">
+        <div className="lg:items-center lg:justify-center flex mt-2 lg:mt-0">
+          <div className="mx-auto">
             <Lottie
               style={{ height: animationHeight }}
               animationData={recipe}
-              loop={0}
+              loop={false}
             />
           </div>
         </div>
--- components/Common/Button.tsx
+++ components/Common/Button.tsx
@@ -29,7 +29,7 @@ function Button({
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`relative disabled:opacity-80 disabled:cursor-not-allowed font-medium rounded-full hover:opacity-80 transition px-5 ${
+      className={`disabled:opacity-80 disabled:cursor-not-allowed font-medium rounded-full hover:opacity-80 transition px-5 flex items-center justify-center gap-3 ${
         full ? 'w-full' : ''
       } ${outline ? 'bg-white' : 'bg-black'} ${outline ? '' : 'border-black'} ${
         outline ? 'text-black' : 'text-white'
@@ -37,12 +37,7 @@ function Button({
       ${small ? 'py-2' : 'py-3.5'}
        ${small ? '' : ''}`}
     >
-      {Icon && (
-        <Icon
-          size={24}
-          className={`absolute left-4 top-3 ${isColor && 'text-blue-600'}`}
-        />
-      )}
+      {Icon && <Icon size={24} className={``} />}
       {loading ? <SmallLoader /> : label}
     </button>
   );
